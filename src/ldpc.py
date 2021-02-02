f72174488dd6003eb0ae201b6457580ce0e5007d
Alban Goupil <77101624+alban-goupil@users.noreply.github.com>
2021-02-02 09:07:23 +0100

    Passe en numba pour accélérer (#2)

--- src/ldpc.py
+++ src/ldpc.py
@@ -1,6 +1,7 @@
 ## Bibliothèque pour le décodage des codes LDPC.
 import re
 import numpy as np
+from numba import jit
 
 p = re.compile('\d+|;|\.')
 
@@ -35,14 +36,13 @@ class LDPC:
           checks.append([])
         else:
           checks[-1].append(tok)
-    self._checks = checks
           
     self._vedges = np.concatenate(checks)
     self._cedges = np.cumsum([0] + list(map(len, checks)))
           
     self.nedges = self._vedges.size
+    self.nchecks = self._cedges.size-1
     self.length = 1 + self._vedges.max()
-    self.nchecks = len(checks)
     self.rate = (self.length - self.nchecks) / self.length
 
     self._v2c = np.zeros(self.nedges) # Messages var -> chk
@@ -52,49 +52,66 @@ class LDPC:
   def check(self, illr):
     """Vérifie si le tableau `illr` contient le LLR d'un mot de
     code."""
-    iscodeword = True
-    for c in range(self.nchecks):
-      for e in range(self._cedges[c], self._cedges[c+1]):
-        if illr[self._vedges[e]] < 0.0:
-          iscodeword = not iscodeword
-      if not iscodeword: return False
-    return iscodeword
+    return numba_check(self._vedges, self._cedges, illr)
 
-
-  def bp(self, illr, maxiter, out=None):
+  
+  def bp(self, illr, ollr, maxiter):
     """Infère le mot de code selon l'algorithme du BP et
-    retourne les LLR de fin de décodage."""
-    if out is None:
-      out = np.zeros_like(illr)
+    retourne dans `ollr` les LLR de fin de décodage et le
+    nombre d'itérations nécessaire.
+    """
+    return numba_bp(self._vedges, self._cedges, self._v2c, self._c2v,
+                    illr, ollr, maxiter)
 
-    # Initialisation  
-    np.copyto(out, illr)
-    for e, v in enumerate(self._vedges): self._v2c[e] = np.tanh(out[v]/2.0)
 
-    # Itération
-    for it in range(maxiter):
-      # Arrêt prématuré
-      if self.check(out):
-        break
 
-      # Check pass
-      for c in range(self.nchecks):
-        for e in range(self._cedges[c], self._cedges[c+1]):
-          m = 1.0
-          for ep in range(self._cedges[c], self._cedges[c+1]):
-            if e != ep:
-              m *= self._v2c[ep]
-          self._c2v[e] = 2.0 * np.arctanh(m)
-              
-      # Data pass
-      np.copyto(out, illr)
-      for e, v in enumerate(self._vedges): out[v] += self._c2v[e]
-      np.negative(self._c2v, self._v2c)
-      for e, v in enumerate(self._vedges): self._v2c[e] += out[v]
-      np.divide(self._v2c, 2.0, out=self._v2c)
-      np.tanh(self._v2c, out=self._v2c)
+
+@jit(nopython=True, fastmath=True)
+def numba_check(vedges, cedges, illr):
+  iscodeword = True
+  for c in range(cedges.shape[0]-1):
+    for e in range(cedges[c], cedges[c+1]):
+      if illr[vedges[e]] < 0.0:
+        iscodeword = not iscodeword
+    if not iscodeword: return False
+  return iscodeword
+
+  
+
+@jit(nopython=True, fastmath=True)
+def numba_bp(vedges,
+             cedges,
+             v2c,
+             c2v,
+             illr,
+             ollr,
+             maxiter):  
+  # Initialisation
+  for v in range(illr.size): ollr[v] = illr[v]
+  for e in range(v2c.size): v2c[e] = np.tanh(ollr[vedges[e]]/2.0)
+
+  # # Itération
+  for it in range(maxiter):
+    # Arrêt prématuré
+    if numba_check(vedges, cedges, ollr):
+      return 1+it
+
+    # Check pass
+    for c in range(cedges.size-1):
+      for e in range(cedges[c], cedges[c+1]):
+        m = 1.0
+        for ep in range(cedges[c], cedges[c+1]):
+          if e != ep:
+            m *= v2c[ep]
+        if m >= 1.0: c2v[e] = 1e300
+        elif m <= -1.0: c2v[e] = -1e300
+        else: c2v[e] = 2.0 * np.arctanh(m)
+
+    # Data pass
+    for v in range(illr.size): ollr[v] = illr[v]
+    for e in range(vedges.shape[0]):
+      ollr[vedges[e]] += c2v[e]
+    for e in range(vedges.shape[0]):
+      v2c[e] = np.tanh((ollr[vedges[e]] - c2v[e])/2.0)
       
-    return 1+it, out
-
-
-ldpc = LDPC(codefile)
+  return maxiter
